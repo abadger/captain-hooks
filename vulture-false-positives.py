f0e59dce6bfdb4d5d9a376c4ce471bd8f7479273
Toshio Kuratomi <a.badger@gmail.com>
2022-03-27 17:11:27 -0700

    Move vulture config into pyproject.toml
    That way it can be run outside of pre-commit.
    
    Also add a file which takes care of false positives

--- vulture-false-positives.py
+++
@@ -1,2 +0,0 @@
-TESTS_PATTERNS  # unused variable (/home/badger/programming/captain-hooks/captain_hooks/meson_file_list.py:40)
-_.format_usage  # unused method (/home/badger/programming/captain-hooks/captain_hooks/vendored/_argparse.py:55)
